77bfb2d2f8af7b003bc4dbed7f2f04ec5f2a7e17
Tom Clarkson <tom@designsight.co.nz>
2014-08-21 16:13:48 +1200

    add routes

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,6 @@
 <?php
 
-Route::get('testworkbenchroute', function() {
-    return 'welcome you made it!';
-});
+Route::get('user/notifications', 'TomClarkson\WebNotifications\WebNotificationsController@getUserNotifications');
+Route::post('user/notifications/{notificationId}/markasread', 'TomClarkson\WebNotifications\WebNotificationsController@markAsRead');
+Route::post('user/notifications/{notificationId}/markasseen', 'TomClarkson\WebNotifications\WebNotificationsController@markAsSeen');
+Route::post('user/notifications/{notificationId}/remove', 'TomClarkson\WebNotifications\WebNotificationsController@remove');
